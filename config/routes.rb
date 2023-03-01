7976e91afbec7e0249a07e25d913d60558ea538d
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-01 20:45:44 +0100

    Added Homepage, Styling for it and the Index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'lists#index'
+
+  root 'lists#home'
+  get 'home', to: 'lists#home'
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
