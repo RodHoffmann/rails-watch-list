8473188b46fad0756b1485127341dd00b114b965
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2023-03-02 10:25:09 +0100

    Added Random Images from Lists to Lists Index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,7 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    @movies_url_array = random_movie_img(@lists)
   end
 
   def show
@@ -24,5 +25,19 @@ class ListsController < ApplicationController
 
   def list_params
     params.require(:list).permit(:name)
+  end
+
+  def random_movie_img(lists)
+    movies_url_array = []
+    unless lists.empty?
+      lists.each do |list|
+        if list.movies.empty?
+          movies_url_array << 'https://nbcpalmsprings.com/wp-content/uploads/sites/8/2021/12/BEST-MOVIES-OF-2021.jpeg'
+        else
+          movies_url_array << list.movies[rand(list.movies.length - 1)].poster_url
+        end
+      end
+    end
+    movies_url_array
   end
 end
